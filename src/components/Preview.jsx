f18c5b8bb8d3e754630614cb6d78f0ef655299a4
Angel Romero <mrangelrcd@gmail.com>
2023-09-11 20:51:00 +0200

    Color change in font when background light/dark

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -6,16 +6,18 @@ export default function Preview({
   styles,
   showPreview,
 }) {
+  if (!showPreview) return;
+
   const fullName = generalState.firstName + " " + generalState.lastName;
   const [color, font] = styles;
-
   const fontStyle = { fontFamily: font, fontSize: "1rem" };
-
-  if (!showPreview) return;
+  const fontColor = isColorDark(color) ? "#ffffff" : "#000000";
 
   return (
     <div className="preview-container" style={fontStyle}>
-      <header className="container" style={{ backgroundColor: color }}>
+      <header
+        className="container"
+        style={{ backgroundColor: color, color: fontColor }}>
         <img src={profilePic} alt="Profile pic" />
         <div>
           <h1>{fullName}</h1>
@@ -42,16 +44,32 @@ export default function Preview({
           {generalState.location}
         </p>
       </section>
-      <DataSection data={experience} title="Experience" color={color} />
-      <DataSection data={education} title="Education" color={color} />
+      {experience.length !== 0 && (
+        <DataSection
+          data={experience}
+          title="Experience"
+          color={color}
+          fontColor={fontColor}
+        />
+      )}
+      {education.length !== 0 && (
+        <DataSection
+          data={education}
+          title="Education"
+          color={color}
+          fontColor={fontColor}
+        />
+      )}
     </div>
   );
 }
 
-function DataSection({ data, title, color }) {
+function DataSection({ data, title, color, fontColor }) {
   return (
     <section className="container info-section">
-      <h3 style={{ backgroundColor: color }} className="section-title">
+      <h3
+        style={{ backgroundColor: color, color: fontColor }}
+        className="section-title">
         {title}
       </h3>
       <ul>
@@ -69,4 +87,23 @@ function DataSection({ data, title, color }) {
       </ul>
     </section>
   );
+}
+
+function isColorDark(color) {
+  // Remove any leading "#" if present
+  color = color.replace(/^#/, "");
+
+  // Parse the color components (red, green, blue)
+  const r = parseInt(color.slice(0, 2), 16);
+  const g = parseInt(color.slice(2, 4), 16);
+  const b = parseInt(color.slice(4, 6), 16);
+
+  // Calculate the luminance using the formula for relative luminance
+  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+  // You can adjust the threshold to your preference (e.g., 0.5 for a mid-point)
+  const threshold = 0.5;
+
+  // Check if the luminance is less than the threshold to determine if it's dark
+  return luminance < threshold;
 }
