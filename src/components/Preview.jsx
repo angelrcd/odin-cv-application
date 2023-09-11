ea9634f356909a9298af093b3c50f48a19918caf
Angel Romero <mrangelrcd@gmail.com>
2023-09-11 20:37:51 +0200

    Style preview section

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -14,18 +14,59 @@ export default function Preview({
   if (!showPreview) return;
 
   return (
-    <div style={fontStyle}>
-      <header style={{ backgroundColor: color }}>
+    <div className="preview-container" style={fontStyle}>
+      <header className="container" style={{ backgroundColor: color }}>
         <img src={profilePic} alt="Profile pic" />
-        <h1>{fullName}</h1>
-        <p>Email: {generalState.email}</p>
-        <p>Phone: {generalState.phone}</p>
-        <p>Location: {generalState.location}</p>
+        <div>
+          <h1>{fullName}</h1>
+          <h2>{generalState.jobTitle}</h2>
+        </div>
       </header>
-      <p>{JSON.stringify(experience)}</p>
-      <p>{JSON.stringify(education)}</p>
-      <p>{color}</p>
-      <p>{font}</p>
+      <section className="contact container">
+        <p>
+          <svg style={{ color: color }}>
+            <use href="/icons/icons.svg#email" />
+          </svg>
+          {generalState.email}
+        </p>
+        <p>
+          <svg style={{ color: color }}>
+            <use href="/icons/icons.svg#phone" />
+          </svg>
+          {generalState.phone}
+        </p>
+        <p>
+          <svg style={{ color: color }}>
+            <use href="/icons/icons.svg#location" />
+          </svg>
+          {generalState.location}
+        </p>
+      </section>
+      <DataSection data={experience} title="Experience" color={color} />
+      <DataSection data={education} title="Education" color={color} />
     </div>
+  );
+}
+
+function DataSection({ data, title, color }) {
+  return (
+    <section className="container info-section">
+      <h3 style={{ backgroundColor: color }} className="section-title">
+        {title}
+      </h3>
+      <ul>
+        {data.map((dataUnit) => {
+          return (
+            <li className="info-unit" key={dataUnit.id}>
+              <h4>
+                {dataUnit.name} ({dataUnit.startDate} - {dataUnit.endDate})
+              </h4>
+              <h5>{dataUnit.organization}</h5>
+              <p>{dataUnit.description}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
